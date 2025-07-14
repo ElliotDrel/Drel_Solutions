be47d819b9f5b8fe3507acac999a36dc7a5de66c
Elliot Drel <524edrel@frhsd.com>
2025-07-14 14:54:40 -0400

    Remove margin and padding from PostGrid component for improved layout consistency

--- src/components/blog/PostGrid.tsx
+++ src/components/blog/PostGrid.tsx
@@ -13,7 +13,7 @@ export const PostGrid = ({
   onTagClick 
 }: PostGridProps) => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-0 pt-0">
       {posts.map((post) => (
         <ArticleCard
           key={post.id}
